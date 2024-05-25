0820a5d630a84126bd324735c11b199828040596
tarekhossen71 <tarekhossen.offi@gmail.com>
2024-05-25 20:02:25 +0600

    useRef input

--- src/components/UseRef.jsx
+++ src/components/UseRef.jsx
@@ -2,15 +2,22 @@ import React, { useRef } from 'react';
 
 const UseRef = () => {
 
-    let myHeading = useRef();
-    let myImg = useRef();
+    // let myHeading = useRef();
+    // let myImg = useRef();
+    let firstName,lastName = useRef();
+    // let lastName = useRef();
 
     const change = () => {
         // myHeading.current.innerText="New heading";
         // myHeading.innerText="New heading";
-        myImg.current.src = 'https://placehold.co/300';
-        myImg.current.setAttribute('width','500px');
-        myImg.current.setAttribute('height','300px');
+        // change attr 
+        // myImg.current.src = 'https://placehold.co/300';
+        // myImg.current.setAttribute('width','500px');
+        // myImg.current.setAttribute('height','300px');
+        // change input 
+        let fName = firstName.value;
+        let lName = lastName.value;
+        alert(fName+' ' +lName);
     }
 
     return (
@@ -20,7 +27,9 @@ const UseRef = () => {
             {/* <h1 ref={(h1)=>myHeading= h1}>Defalut heading</h1>
              */}
 
-             <img ref={myImg} src="https://placehold.co/300x200" alt="" />
+             {/* <img ref={myImg} src="https://placehold.co/300x200" alt="" /> */}
+             <input type="text" ref={(a)=>firstName=a} name='fname' placeholder='Enter First Name' />
+             <input type="text" ref={(b)=>lastName=b} name='lname' placeholder='Enter Last Name' />
             <button onClick={change}>Click</button>
         </div>
     );
