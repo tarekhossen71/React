193ff8278fb8e6b86a210dab20b026255cee6d5f
tarekhossen71 <tarekhossen.offi@gmail.com>
2024-05-27 23:20:55 +0600

    useState Object

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from 'react';
 
 const UseState = () => {
-    let [num,setNum] = useState(0);
-    const change =()=>setNum(num+1)
+    // let [num,setNum] = useState(0);
+    // const change =()=>setNum(num+1)
+
+
+    let [obj,setObj] = useState({
+        key1:'Value 1',
+        key2:'Value 2',
+        key3:'Value 3',
+    });
+    const change = () => {
+        setObj(
+            previousObj=>({
+                ...previousObj,
+                key1:'New Value for key1'
+            })
+        )
+    }
+    
     return (
         <div>
             <div className='text-center'>
-                <h1>You have clicked {num} times</h1>
-                <button onClick={change} className='btn btn-primary'>Count</button>
+                {/* <h1>You have clicked {obj.key1} times</h1> */}
+                {/* <button onClick={change} className='btn btn-primary'>Count</button> */}
+                <h1>{ obj.key1 }</h1>
+                <button onClick={change} className='btn btn-primary'>Change</button>
             </div>
         </div>
     );
