4128b19cf2ea069f9bae05a172c0cc80a6da1fe5
tarekhossen71 <tarekhossen.offi@gmail.com>
2024-05-27 23:11:50 +0600

    useState

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 
 const UseState = () => {
     let [num,setNum] = useState(0);
-
+    const change =()=>setNum(num+1)
     return (
         <div>
             <div className='text-center'>
                 <h1>You have clicked {num} times</h1>
-                <button onClick={()=>setNum(num+1)} className='btn btn-primary'>Count</button>
+                <button onClick={change} className='btn btn-primary'>Count</button>
             </div>
         </div>
     );
